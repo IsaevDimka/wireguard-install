ecd8e186729929a0f709d87030e20d7ed2d84fb9
Isaev Dimka <isaevdimka@gmail.com>
2022-06-05 18:57:42 +0300

    feat: add wireguard-install.sh
    Signed-off-by: Isaev Dimka <isaevdimka@gmail.com>

--- wireguard-install.sh
+++ wireguard-install.sh
@@ -2,7 +2,7 @@
 #
 # https://github.com/IsaevDimka/wireguard-install
 #
-# Copyright (c) 2020 Nyr. Released under the MIT License.
+# Copyright (c) 2022 IsaevDimka. Released under the MIT License.
 
 
 # Detect Debian users running the script with "sh" instead of bash
